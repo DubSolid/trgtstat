326f4668b2f32d54b3df13685b879860884ffdaa
Chris <71638505+DubSolid@users.noreply.github.com>
2023-08-31 11:14:55 +0200

    Update trgtstat.go
    Added the option to choose between just ping, just traceroute or both

--- trgtstat.go
+++ trgtstat.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"os/exec"
-	"strings"
+    "strings"
 )
 
 var pingTargets = []string{
@@ -12,13 +12,44 @@ var pingTargets = []string{
 } 
 
 func main() { 
-    for _, ip := range pingTargets {
-        if ipPing(ip) {
-            fmt.Printf("\u2713 %s is reachable\n", ip)
-        } else {
-            fmt.Printf("\u2717 %s is unreachable\n", ip)   
+    var choice int
+    for {
+        fmt.Println("--- Target Status ---")
+        fmt.Println("1. Ping targets")
+        fmt.Println("2. Traceroute on targets")
+        fmt.Println("3. Both")
+        fmt.Println("4. Exit")
+	fmt.Print("Choose and option: ")
+
+        fmt.Scanln(&choice)
+        switch choice {
+        case 1:      
+            for _, ip := range pingTargets {
+                if ipPing(ip) {
+                    fmt.Printf("\u2713 %s is reachable\n", ip)
+                } else {
+                    fmt.Printf("\u2717 %s is unreachable\n", ip)   
+                }
+            }
+        case 2:
+            for _, ip := range pingTargets {
+                traceRouteOnly(ip)
+            }
+        case 3:
+            for _, ip := range pingTargets {
+                if ipPing(ip) {
+                    fmt.Printf("\u2713 %s is reachable\n", ip)
+                } else {
+                    fmt.Printf("\u2717 %s is unreachable\n", ip)   
+                }
+            traceRoute(ip)
+            }
+        case 4:
+            fmt.Println("Exitingg...")
+            return
+
         }
-        traceRoute(ip)
+        break
     }
 }
 
@@ -36,6 +67,23 @@ func ipPing(ip string) bool {
     fmt.Println()
     return true
 }
+
+func traceRouteOnly(ip string) bool {
+    fmt.Println(strings.Repeat("_", 80))
+    fmt.Printf("Executing traceroute on selected targets...\n")
+    cmd := exec.Command("traceroute", ip)
+
+    output, err := cmd.CombinedOutput()
+    if err != nil {
+        fmt.Println("Failed to execute traceroute: ", err)
+        return false
+    }
+    
+    fmt.Println(string(output))
+    fmt.Println()
+    return true
+}
+
 
 func traceRoute(ip string) bool {
     fmt.Println(strings.Repeat("_", 80))
